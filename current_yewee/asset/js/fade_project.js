55da6355f4841bdd1a5813fa73217b244c5b2b9f
Benoit <benoit.lamonica@gmail.com>
2018-04-16 23:19:04 +0200

    IULAH TROP LONG

--- current_yewee/asset/js/fade_project.js
+++ current_yewee/asset/js/fade_project.js
@@ -1,6 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // effet fondu en entrée à l'ouverture de la page
-    $('#project_page').fadeIn(1000);
+    $('#project_page').fadeIn(3000);
+    $("#ico").hide();
+    $("#accueil").hide();
+    $("#streamer").hide();
+    $("#live").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#ico').fadeIn(1500);
+        } else {
+            $('#ico').fadeOut(100);
+        }
+        if ($(this).scrollTop() > 850) {
+            $('#accueil').fadeIn(1500);
+        } else {
+            $('#accueil').fadeOut(100);
+        }
+        if ($(this).scrollTop() > 1650) {
+            $('#streamer').fadeIn(1500);
+        } else {
+            $('#streamer').fadeOut(100);
+        }
+        if ($(this).scrollTop() > 2450) {
+            $('#live').fadeIn(1500);
+        } else {
+            $('#live').fadeOut(100);
+        }
+    });
 
 });
