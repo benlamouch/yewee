f600b7605bc471b6368f7806b14789b517128d6d
felixbouveret <bouveret.felix@gmail.com>
2018-07-02 09:13:58 +0200

    jsplus ce que c'est

--- current_yewee/asset/js/fade_team.js
+++ current_yewee/asset/js/fade_team.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
 
     // effet fondu en entrée à l'ouverture de la page
     $('#team_section').fadeIn(1000);
+    $('#about_section').fadeIn(1000);
+    $('#about_footer').fadeIn(1000);
 
 });
---
+++ current_yewee/asset/js/script_progress.js
@@ -0,0 +1,8 @@
+$('.site_content_contain_progress').hide()
+
+$(document).ready(function() {
+
+    // effet fondu en entrée à l'ouverture de la page
+    $('.site_content_contain_progress').fadeIn(1000);
+
+});
